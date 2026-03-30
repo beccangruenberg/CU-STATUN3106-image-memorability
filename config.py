ca33648f7bbf9b921d55dd0a537e00d6b086bbca
beccangruenberg <git@beccan.gruenbergs.net>
2026-03-30 17:42:51 -0400

    initial ml_pipeline.ipynb

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-# Fill in your own paths below before running any notebooks
-# Then run: git update-index --skip-worktree config.py
-
-DATA_PATH = ""  # e.g. "/content/drive/MyDrive/YourSharedFolderName/"
